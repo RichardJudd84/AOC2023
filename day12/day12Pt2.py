2d6c10d7cea006d7b587dc9b17e719167a4c2e26
Richard Judd <richard.judd@sveaskog.se>
2023-12-27 10:03:53 +0100

    Day 12 pt 2

--- day12/day12Pt2.py
+++ day12/day12Pt2.py
@@ -1,80 +1,76 @@
 import queue
 import re
-with open("sampleData.txt", "r" ) as data:
-#with open("input.txt", "r" ) as data:
+#with open("testData.txt", "r" ) as data:
+#with open("sampleData.txt", "r" ) as data:
+with open("input.txt", "r" ) as data:
     res = data.readlines()
 
-answer = 0    
-repp = 0
+############################################### helpers ###############################################
+def findAllPositions(sqVals, sqI, ln:str, i):
+    hashPos = None
+    validPositions = []
+    sqVal = sqVals[sqI]
+    minLnRemaining = sum(sqVals[sqI+1:]) + len(sqVals[sqI+1:])
+    sumRemainingSq = sum(sqVals[sqI+1:])
+
+    
+    while minLnRemaining < len(ln[i+sqVal:]):        
+        subLn = ln[i:i+sqVal]
+        next = ln[i+sqVal]
+        previous = ln[i-1]
+        
+        if not hashPos and '#' in subLn:
+            hashPos = i+subLn.find('#')
+
+        elif hashPos and i > hashPos:
+            return validPositions
+        
+        if '.' not in subLn and next != '#' and previous != '#':
+            remainingLn = ln[i+sqVal:]
+            hashesRemaining = len(re.findall('#', remainingLn))
+            if hashesRemaining <=  sumRemainingSq:
+                validPositions.append((i, i+sqVal))
+        i += 1
+    return validPositions
+
+def addListToDict(primaryDict: dict(), listToAdd: list(), multiple=1):
+    for key in listToAdd:
+        if key in primaryDict:
+            primaryDict[key] += multiple
+        else:
+            primaryDict[key] = multiple
+
+
+############################################### execution ###############################################
+            
+nCopies = 4
+answer = 0
+
 for row in res:
-    repp+=1
     row = row.strip().split()
     sequence = [int(e) for e in row[1].split(',')]
     line = row[0]
-    tsequence = sequence.copy()
-    for rep in range(2):
+    sequenceCopy = sequence.copy()
+
+    # fold out map with n folds
+    for rep in range(nCopies):
         line += '?' + row[0]
-        sequence += tsequence
+        sequence += sequenceCopy
 
-    print(line, sequence)
-    # remove leading and trailing '.' from line
+    # clean up line by replacing multiple '...' with '.'
+    ln = '.' + line +'.'
+    ln = re.sub('[.]+', '.', ln)
 
-    line = line.strip('.')
-    print(line)
+    # for each sequence val find all valid possisions and add to dictionary of next possible positions with their multiples
+    currentPositions = dict()
+    initialPositions = findAllPositions(sequence, 0, ln,1)
+    addListToDict(currentPositions, initialPositions, 1)
 
-    # get list all parts
-    parts = re.findall('[^.]+', line)
-    print('List of all parts: ', parts)
-
-    print(parts)
-    print(sequence)
-
+    for i in range(1,len(sequence)):
+        nextPositions = dict()
+        for pos in currentPositions: 
+            addListToDict(nextPositions,findAllPositions(sequence, i, ln, pos[1]+1),currentPositions[pos])
+        currentPositions = nextPositions
     
-    
-
-    # find possible sequence numbers per part from left to right
-    numsLToR = []
-    seqI = 0
-    for part in parts:
-        partNums = []
-        sumSeqNR = sequence[seqI]
-        size = len(part)
-        while sumSeqNR <= size:
-            #print('ltr' , seqI)
-            partNums.append(seqI)            
-            if seqI+1 >= len(sequence):
-                break
-            seqI += 1
-            sumSeqNR += 1 + sequence[seqI]
-        numsLToR.append(partNums)        
-    
-
-    #find possible sequence numbers per part from right to left
-    parts.reverse() 
-    numsRToL = []
-    for part in parts:
-        partNums = []
-        sumSeqNR = sequence[seqI]
-        
-        size = len(part)
-        while sumSeqNR <= size:
-            #print('rtl', seqI)
-            partNums.append(seqI)
-            if seqI <= 0:
-                break
-            seqI -= 1
-            sumSeqNR += 1 + sequence[seqI]
-        partNums.reverse()    
-        numsRToL.append(partNums)
-    numsRToL.reverse()
-    parts.reverse()
-
-
-    for l, r , p in zip(numsLToR, numsRToL, parts):
-        print(l,r, p)
-
-
-    input()
-
-    
-      
+    answer += sum(currentPositions.values())
+print(answer)
